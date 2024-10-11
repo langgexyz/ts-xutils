f9a316ae39d429e3851b7d4f6f8edba934c5ee07
xpwu <diodexp@foxmail.com>
2024-10-12 00:13:29 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 
 export {type Duration, Hour, Second, Minute, Microsecond, Millisecond} from "./src/duration"
 
+export {Utf8} from "./src/utf8"
 
+export {type Logger, ConsoleLogger} from "./src/logger"
