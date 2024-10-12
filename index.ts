81f5f588b18fedc0e52804576ec254affcaa9d2f
xpwu <diodexp@foxmail.com>
2024-10-12 12:45:59 +0800

    add assert

--- index.ts
+++ index.ts
@@ -4,3 +4,5 @@ export {type Duration, Hour, Second, Minute, Microsecond, Millisecond} from "./s
 export {Utf8} from "./src/utf8"
 
 export {type Logger, ConsoleLogger} from "./src/logger"
+
+export {AssertError, assert} from "./src/assert"
